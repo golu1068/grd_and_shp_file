4023946ff9d949a3940126e0c4e7fc4353ae60c2
golu1068 <43088939+golu1068@users.noreply.github.com>
2020-05-22 15:23:16 +0530

    Update and rename inte.py to free_inte.py

--- free_inte.py
+++ free_inte.py
@@ -1,19 +1,21 @@
 import requests
 from selenium import webdriver
 from struct import unpack
-##from pandas import DataFrame, read_csv
+from pandas import DataFrame, read_csv
 from os.path import split, join
 import os
 import tempfile
 from glob import glob
-##import pandas as pd
-##from free_function import read_grd_rain, read_grd_temp
+import pandas as pd
+from free_function import read_grd_rain, read_grd_temp, mean
 from selenium.webdriver.support.ui import Select
+import numpy as np
 #############################################################################
 result_dir = os.getcwd()
 ###################################
 t = tempfile.TemporaryDirectory(dir = tempfile.gettempdir())
 print(t.name)
+
 rainfall_url = 'http://www.imdpune.gov.in/Clim_Pred_LRF_New/rainfall.php'
 maxtemp_url = 'http://www.imdpune.gov.in/Clim_Pred_LRF_New/maxtemp.php'
 mintemp_url = 'http://www.imdpune.gov.in/Clim_Pred_LRF_New/mintemp.php'
@@ -90,49 +92,65 @@ if mintemp == 'y':
 ####  For rainfall
 ##dir = os.getcwd()
 os.chdir(t.name)
-files = glob('*.grd')  
+curr_wd = os.getcwd()
+files = glob(curr_wd + r'\rain*.grd')  
+
 if (len(files) == 0):
     print('No any grd file in this folder')
 for file in files:
-    if (file[0:4] == 'rain'):
+    if (os.path.split(file)[1][0:4] == 'rain'):
         print(file)
         year = file[-8:-4]
         print(year)
         read_grd_rain(file, year, result_dir)
-    else:
-        print('Grd filename is not proper')
-    break
+
 print('Conversion for Rainfall complete')
-###################################################################################################
+##################################################################################################
 ####  For temp
 ####dir = os.getcwd()
 os.chdir(t.name)
-files = glob('*.GRD')
-if (len(files) == 0):
+curr_wd = os.getcwd()
+files_max = glob(curr_wd + r'\maxtemp*.GRD')
+files_min = glob(curr_wd + r'\mintemp*.GRD')
+if (len(files_max) == 0):
     print('No any grd file in this folder')
-for file in files:
-    if (file[0:7] == 'maxtemp' or file[0:7] == 'mintemp'):
-        print(file)
-        year = file[-8:-4]
-        read_grd_temp(file, year, result_dir)
+for file in files_max:
+    ### For max temp file
+    file_name = os.path.split(file)[1]
+    year = file_name[-8:-4]
+    df_max = read_grd_temp(file, year, result_dir)
+    print('max comp')
+    ###########################################################################
+    ## For min temp file
+    file = curr_wd + '\mintemp_'+ file[-8:]
+    if ((file in files_min) == True):
+        df_min = read_grd_temp(file, year, result_dir)
+        print('min comp')
     else:
-        print('Grd filename is not proper')
+        print('Mintemp file is not there for '+file[-8:-4]+' year')
+    ##############################################################################
+    ### For Mean
+    curr_wd = os.getcwd()
+    
+    mean_file = os.path.join(curr_wd, 'mean_temp_'+str(year)+'.csv')
+    mean_file_csv = open(mean_file, 'w', newline='')
+    
+    df_mean = pd.DataFrame()
+    df_header = list(df_min)[2:]
+    
+    df_mean.insert(0, 'Latitude', df_min['Latitude'][0:961])
+    df_mean.insert(1, 'Longitude', df_min['Longitude'][0:961])
+    
+    for i in range(len(df_header)):
+        a = np.array(df_min[df_header[i]])
+        b = np.array(df_max[df_header[i]])
+        c = (a + b)/2
+        header = list(df_mean)
+        df_mean.insert(len(header),  'day_'+str('%s'%(i+1)), c)
+    df_mean.to_csv(mean_file_csv, index=False)
+    ###############################################################################
+    print('Conversion to mean complete')
     
 print('Conversion for Temp complete')
-############################################################################################
-####   Temp mean
-years = temp_range
-max_file='';min_file=''
-dir = result_dir + '/Result'
-os.chdir(dir)
-for year in years:
-    files = glob('*'+str(year)+'.csv')
-    for file in files:
-        #print(file)
-        if (file[0:7] == 'maxtemp'):
-            max_file = file
-        if (file[0:7] == 'mintemp'):
-            min_file = file
-        if (min_file != '' and max_file != ''):
-            mean(max_file, min_file, year)
-            break
+############################################################################################  
+ 
