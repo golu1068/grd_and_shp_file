79c6f59471a99ef93cf2f1366180dd19704e8b6f
nagendra rai <43088939+golu1068@users.noreply.github.com>
2020-06-01 15:20:39 +0530

    Add files via upload

--- free_inte.py
+++ free_inte.py
@@ -7,12 +7,12 @@ import os
 import tempfile
 from glob import glob
 import pandas as pd
-from free_function import read_grd_rain, read_grd_temp, mean
+from free_function import read_grd_rain, read_grd_temp, csv2shp
 from selenium.webdriver.support.ui import Select
 import numpy as np
 #############################################################################
 result_dir = os.getcwd()
-###################################
+###########################################################
 t = tempfile.TemporaryDirectory(dir = tempfile.gettempdir())
 print(t.name)
 
@@ -58,7 +58,7 @@ if mintemp.lower() == 'y':
     mintemp_start = input("Please enter start year for Min. Temp. : ")
     mintemp_stop = input("Please enter stop year for Min. Temp. : ")
 ###########################################################
-temp_range = list(range(maxtemp_start, maxtemp_stop+1))
+temp_range = list(range(int(maxtemp_start), int(maxtemp_stop)+1))
 #######################################################
 if rainfall == 'y':
     for x in range(int(rainfall_start),int(rainfall_stop)+1):
@@ -74,6 +74,7 @@ if maxtemp == 'y':
     for x in range(int(maxtemp_start),int(maxtemp_stop)+1):
         data = {'maxtemp':str(x)}
         res = requests.post(maxtemp_url, headers=headers, data=data, timeout=15)
+        os.chdir(t.name)
         with open('maxtemp_'+str(x)+'.GRD','wb')as file:
             file.write(res.content)
         print('Downloaded Max. Temp. - '+str(x))
@@ -83,46 +84,51 @@ if mintemp == 'y':
     for x in range(int(mintemp_start),int(mintemp_stop)+1):
         data = {'mintemp':str(x)}
         res = requests.post(mintemp_url, headers=headers, data=data, timeout=15)
+        os.chdir(t.name)
         with open('mintemp_'+str(x)+'.GRD','wb')as file:
             file.write(res.content)
         print('Downloaded Min. Temp. - '+str(x))
-
-
+print('done')
 ###############################################################################################
 ####  For rainfall
-##dir = os.getcwd()
-os.chdir(t.name)
-curr_wd = os.getcwd()
-files = glob(curr_wd + r'\rain*.grd')  
+#dir = os.getcwd()
+#os.chdir(t.name)
+#curr_wd = os.getcwd()
+#files = glob(curr_wd + r'/rain*.grd')  
 
-if (len(files) == 0):
-    print('No any grd file in this folder')
-for file in files:
-    if (os.path.split(file)[1][0:4] == 'rain'):
-        print(file)
-        year = file[-8:-4]
-        print(year)
-        read_grd_rain(file, year, result_dir)
-
+#if (len(files) == 0):
+   # print('No any grd file in this folder')
+#for file in files:
+ #   if (os.path.split(file)[1][0:4] == 'rain'):
+ #       print(file)
+ #       year = file[-8:-4]
+  #      print(year)
+  #      df_rain = read_grd_rain(file, year, result_dir)
+        
+  #  csv2shp(df_rain, result_dir, year, 'rain')
 print('Conversion for Rainfall complete')
-##################################################################################################
+####################################################################
 ####  For temp
 ####dir = os.getcwd()
 os.chdir(t.name)
 curr_wd = os.getcwd()
-files_max = glob(curr_wd + r'\maxtemp*.GRD')
-files_min = glob(curr_wd + r'\mintemp*.GRD')
+files_max = glob(curr_wd + r'/maxtemp*.GRD')
+files_min = glob(curr_wd + r'/mintemp*.GRD')
 if (len(files_max) == 0):
     print('No any grd file in this folder')
 for file in files_max:
-    ### For max temp file
+    os.chdir(t.name)
+    curr_wd = os.getcwd()
+    ### For max temp filex
     file_name = os.path.split(file)[1]
+    print(file_name)
     year = file_name[-8:-4]
     df_max = read_grd_temp(file, year, result_dir)
     print('max comp')
     ###########################################################################
     ## For min temp file
-    file = curr_wd + '\mintemp_'+ file[-8:]
+    file = curr_wd + '/mintemp_'+ file[-8:]
+    print('mintemp_'+ file[-8:])
     if ((file in files_min) == True):
         df_min = read_grd_temp(file, year, result_dir)
         print('min comp')
@@ -135,11 +141,19 @@ for file in files_max:
     mean_file = os.path.join(curr_wd, 'mean_temp_'+str(year)+'.csv')
     mean_file_csv = open(mean_file, 'w', newline='')
     
+    equ_file = os.path.join(curr_wd, 'equation_'+str(year)+'.csv')
+    equ_file_csv = open(equ_file, 'w', newline='')
+    
     df_mean = pd.DataFrame()
     df_header = list(df_min)[2:]
     
+    df_equ = pd.DataFrame()
+    
     df_mean.insert(0, 'Latitude', df_min['Latitude'][0:961])
     df_mean.insert(1, 'Longitude', df_min['Longitude'][0:961])
+    
+    df_equ.insert(0, 'Latitude', df_min['Latitude'][0:961])
+    df_equ.insert(1, 'Longitude', df_min['Longitude'][0:961])
     
     for i in range(len(df_header)):
         a = np.array(df_min[df_header[i]])
@@ -147,10 +161,29 @@ for file in files_max:
         c = (a + b)/2
         header = list(df_mean)
         df_mean.insert(len(header),  'day_'+str('%s'%(i+1)), c)
-    df_mean.to_csv(mean_file_csv, index=False)
+        
+        diff = b-a
+        ##########################################################################
+        ### condition
+        diff[np.absolute(diff)>700] = 0
+        diff = np.sqrt((diff))
+        diff = np.nan_to_num(diff)
+        #############################################################################
+        
+        ra = 10
+        equ = 0.0022*ra*(diff)*(c + 17.8)
+        
+        header = list(df_equ)
+        df_equ.insert(len(header),  'day_'+str('%s'%(i+1)), equ)
+        
+#    df_mean.to_csv(mean_file_csv, index=False)
+#    df_equ.to_csv(equ_file_csv, index=False)
+    
+    csv2shp(df_equ, result_dir, year, 'temp')
     ###############################################################################
     print('Conversion to mean complete')
     
 print('Conversion for Temp complete')
-############################################################################################  
- 
+## Clean temp file
+t.cleanup()
+############################################################################################ 
